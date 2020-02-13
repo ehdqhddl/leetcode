b92a010a57bca7cf2958b156f9e3afd597dc15b4
DongBongE <55652438+ehdqhddl@users.noreply.github.com>
2020-02-14 00:49:43 +0900

    leetcodeBinaryTreePreorderTraversal
    leetcodeBinaryTreePreorderTraversal

--- leetcodeBinaryTreePreorderTraversal/src/Solution.java
+++ leetcodeBinaryTreePreorderTraversal/src/Solution.java
@@ -1,14 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
+import java.util.ArrayList;
+import java.util.List;
 
+
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<Integer> traversal(TreeNode node, List<Integer> lst){
+        if(node == null){
+            return lst;
+        }
+        else{
+            lst.add(node.val);
+            lst = traversal(node.left,lst);
+            lst = traversal(node.right,lst);
+            return lst;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> lst = new ArrayList<Integer>();
+
+        return traversal(root,lst);
     }
 }
